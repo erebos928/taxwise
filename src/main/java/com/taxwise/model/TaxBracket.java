af5d6556f9e2d2c2a9e6c56d0fe03d4e66d40c99
tsagh <valesafeudjio@gmail.com>
2025-07-02 22:59:01 -0400

    les classes du model ont été complétés

--- src/main/java/com/taxwise/model/TaxBracket.java
+++ src/main/java/com/taxwise/model/TaxBracket.java
@@ -1,4 +1,25 @@
 package com.taxwise.model;
 
 public class TaxBracket {
+    private double minIncome;
+    private double maxIncome;
+    private double taxRate;
+
+    public TaxBracket(double minIncome, double maxIncome, double taxRate) {
+        this.minIncome = minIncome;
+        this.maxIncome = maxIncome;
+        this.taxRate = taxRate;
+    }
+
+    public double getMinIncome() {
+        return minIncome;
+    }
+
+    public double getMaxIncome() {
+        return maxIncome;
+    }
+
+    public double getTaxRate() {
+        return taxRate;
+    }
 }
