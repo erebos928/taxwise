b4f817afc3e52a8602556820f8cd201580615a47
tsagh <valesafeudjio@gmail.com>
2025-07-05 19:06:02 -0400

    les classe taxpayer complétée

--- src/main/java/com/taxwise/model/TaxPayer.java
+++ src/main/java/com/taxwise/model/TaxPayer.java
@@ -1,4 +1,20 @@
 package com.taxwise.model;
 
 public class TaxPayer {
+
+        private String name;
+        private int sin; // numéro d'assurance sociale
+
+        public TaxPayer(String name, int sin) {
+            this.name = name;
+            this.sin = sin;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getSin() {
+            return sin;
+        }
 }
